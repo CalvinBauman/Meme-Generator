9d9b706667169a255819bd5244da83fcc947b06e
allenseb <allenseb38@gmail.com>
2022-08-18 19:41:37 -0700

    updatejs

--- script.js
+++ script.js
@@ -1,37 +1,49 @@
-const generateCelebBtn = document.querySelector(".celeb-generator .generate-celeb-btn");
-const celebImage = document.querySelector(".celeb-generator img");
-const celebTitle = document.querySelector(".celeb-generator .parent-title");
-const celebTitleTwo = document.querySelector(".celeb-generator .parent-titleTwo");
-const generateCelebTwoBtn = document.querySelector(".celeb-generator .generate-celebTwo-btn");
+const generateCaptionBtn = document.querySelector(".generate-cap-btn");
+const memeImage = document.querySelector(".caption-gen");
+//const celebTitle = document.querySelector(".celeb-generator .parent-title");
+const memeSpot = document.querySelector("#memeImage");
+const generateMemeBtn = document.querySelector(".flex-container .generate-meme-btn");
 
+var randomNumber = 1
 
-const updateDetails = (url, title) => {
-    celebImage.setAttribute("src", url);
-    celebTitle.innerHTML = title;
+const updateDetails = (title) => {
+    memeImage.textContent = title;
+    //celebTitle.innerHTML = title;
 
 }
 const updateDetailsTwo = (url, title) => {
-    celebImage.setAttribute("src", url);
-    celebTitleTwo.innerHTML = title;
+    randomInt();
+    //console.log(randomNumber);
+    memeSpot.setAttribute("src", url);
+    //celebTitleTwo.innerHTML = title;
+    // put randomInt() line 15 to implement function from 38-40 /
 
 }
 
-const generateCeleb = () => {
-    fetch("https://meme-api.herokuapp.com/gimme/wholesomememes")
+const generateCaption = () => {
+    console.log("hello")
+    fetch("https://api.quotable.io/random")
     .then((response) => response.json())
     .then(data => {
-        updateDetails(data.url, data.title)
+        updateDetails(data.content)
     });
 }
-const generateCelebTwo = () => {
-    console.log(celebTitleTwo);
+const generateMeme = () => {
+    console.log(memeSpot);
     fetch("https://api.imgflip.com/get_memes")
     .then((response) => response.json())
     .then(data => {
         console.log(data);
-        updateDetailsTwo(data.data.memes[0].url, data.data.memes[0].name)
+       
+        updateDetailsTwo(data.data.memes[randomNumber].url, data.data.memes[randomNumber].name)
     });
 }
 
-generateCelebBtn.addEventListener("click", generateCeleb);
-generateCelebTwoBtn.addEventListener("click", generateCelebTwo);
+const randomInt = () => {
+    randomNumber = Math.floor(Math.random()* 101);
+}
+
+//line 38-40 allows random meme to be pulled from api instead of set number
+
+generateCaptionBtn.addEventListener("click", generateCaption);
+generateMemeBtn.addEventListener("click", generateMeme);
